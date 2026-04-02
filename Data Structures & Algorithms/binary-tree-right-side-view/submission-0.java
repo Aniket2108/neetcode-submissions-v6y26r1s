52354a168217ac8bf4561559d645ab8aebb8ccec
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-02 23:03:37 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preOrder(root,list,0);
+        return list;
+    }
+
+    public void preOrder(TreeNode root,List<Integer> list,int depth){
+        if(root == null){
+            return;
+        }
+        if(list.size() == depth){
+            list.add(root.val);
+        }
+        preOrder(root.right,list,depth+1);
+        preOrder(root.left,list,depth+1);
+    }
+}
