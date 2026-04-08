7d8262089c1ba92ae616c1275d53bc5f149cdf48
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-08 08:49:17 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<prerequisites.length;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int[] inDegree = new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            for(int it:adj.get(i)){
+                inDegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        List<Integer> topo = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            topo.add(node);
+
+            for(int it:adj.get(node)){
+                inDegree[it]--;
+                if(inDegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+
+        if (topo.size() == numCourses) {
+            int[] res = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                res[i] = topo.get(i);
+            }
+            return res;
+        }
+
+        return new int[]{};
+    }
+}
