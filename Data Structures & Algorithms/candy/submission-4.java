d5e30c545016a9d983a386fcc8f4c2ef0d4e37ee
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-13 23:51:30 +0530

    Add: candy - submission-4

---
+++ Data Structures & Algorithms/candy/submission-4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int i = 1;
+        int sum = 1;
+
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                sum = sum + 1;
+                continue;
+            }
+
+            int up = 0;
+            while(i < n && ratings[i] > ratings[i-1]){
+                up++;
+                sum += up + 1;
+                i++;
+            }
+
+            int down = 0;
+            while(i < n && ratings[i] < ratings[i-1]){
+                down++;
+                sum += down;
+                i++;
+            }
+
+            if(down >= up){
+                sum += (down - up);
+            }
+        }
+        return sum;
+    }
+}
