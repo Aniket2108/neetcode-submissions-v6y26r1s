9ea6c64bbfc687e643c06c2f7d3bb5e287177376
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-13 23:39:15 +0530

    Add: candy - submission-1

---
+++ Data Structures & Algorithms/candy/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] left = new int[n];
+        left[0] = 1;
+
+        for(int i=1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }
+            else{
+                left[i] = 1;
+            }
+        }
+        
+        int sum = Math.max(left[n-1],1);
+        int cur = 1;
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1]){
+                cur = cur + 1;
+            }
+            else{
+                cur = 1;
+            }
+            sum = sum + Math.max(cur,left[i]);
+        }
+        return sum;
+    }
+}
