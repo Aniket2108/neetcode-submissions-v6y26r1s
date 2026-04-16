143c9857e559c71bf9ab7050dbb991da11786c91
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-17 00:55:14 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+
+        while(n < 0){
+            ans = ans*(1/x);
+            n++;
+        }
+
+        while(n > 0){
+            ans = ans * x;
+            n--;
+        }
+        return ans;
+    }
+}
