3d1e58b642d474bc6aaef82729ab8eb9d93cf95a
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-21 23:22:06 +0530

    Add: edit-distance - submission-5

---
+++ Data Structures & Algorithms/edit-distance/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+        int[] prev = new int[m+1];
+
+        for(int j=0;j<=m;j++) prev[j] = j;
+
+        for(int i=1;i<=n;i++){
+            int[] cur = new int[m+1];
+            cur[0] = i;
+            for(int j=1;j<=m;j++){
+               if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    cur[j] = prev[j-1];
+                }
+                else cur[j] =Math.min(1+prev[j],Math.min(1+cur[j-1],1+prev[j-1])); 
+            }
+            prev = cur;
+        }
+
+        return prev[m];
+    }
+
+}
