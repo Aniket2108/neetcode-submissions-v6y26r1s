27338ca7d42ed358d5b804acfa2a34ce86c23e6b
Aniket2108 <80541138+Aniket2108@users.noreply.github.com>
2026-04-24 00:05:44 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+       int[][] dp = new int[n+2][2];
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int idx=n-1;idx>=0;idx--){
+            for(int buy=0;buy<2;buy++){
+                if(buy == 1){
+            dp[idx][buy] = Math.max(-prices[idx] + dp[idx+1][0],dp[idx+1][1]);
+        }
+        else{
+            dp[idx][buy] = Math.max(prices[idx] + dp[idx+2][1],dp[idx+1][0]);
+        }
+            }
+        }
+
+        return dp[0][1];
+    }
+
+}
